b30cb5d9e7b88b41e687c7952ec8b9b781082345
Sagar Patil <sagarreddypatil@gmail.com>
2024-04-29 21:02:12 -0400

    start my fork

--- controllers.py
+++ controllers.py
@@ -11,9 +11,14 @@ class OpenController(BaseController):
 class SimpleController(BaseController):
   def update(self, target_lataccel, current_lataccel, state):
     return (target_lataccel - current_lataccel) * 0.3
+  
+class LazyController(BaseController):
+  def update(self, target_lataccel, current_lataccel, state):
+    return 0
 
 
 CONTROLLERS = {
   'open': OpenController,
   'simple': SimpleController,
+  'lazy': LazyController,
 }
